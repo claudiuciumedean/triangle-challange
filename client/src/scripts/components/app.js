b5aabefd8c32fba62ff543e98765721ad5b59662
claudiuciumedean <claudiuciumedean@Claudius-MacBook-Pro.local>
2019-02-10 21:01:47 +0100

    minor addings

--- client/src/scripts/components/app.js
+++ client/src/scripts/components/app.js
@@ -4,6 +4,7 @@ export default class App {
     constructor(el) {
         this.el = el;
         this.inputs = [...this.el.querySelectorAll(".input")];
+        this.tsApp = ts.ui.get(".ts-app", app => app);
 
         this.el.querySelector(".submit-button").addEventListener("click", this.calculate.bind(this));
     }
@@ -15,7 +16,7 @@ export default class App {
             const input = this.inputs[i];
             const val = input.value;
 
-            if(!val || isNaN(parseInt(val))) { 
+            if(isNaN(parseInt(val))) { 
                 ts.ui.Notification.warning(messages.invalidData);
                 obj = null;
                 break;
@@ -31,19 +32,20 @@ export default class App {
         const obj = this.getData();
         if(!obj) { return; }
 
-        this.toggleSpinner(true);
+        this.tsApp.blocking(messages.loading);
 
         await fetch("/check-triangle", {
             method: "POST", 
             body: JSON.stringify(obj), 
             headers: { "content-type": "application/json" }
         }).then(response => {
-            this.toggleSpinner(false);
+            this.tsApp.done();
+
             if(response.status !== 200) {
                 ts.ui.Notification.error(messages.invalidRequest);
                 return;
             }
-            
+
             response.json().then(data => this.processData(data));
         });
     }
@@ -55,17 +57,6 @@ export default class App {
         }
         
         ts.ui.Notification.info(data.message);
-    }
-
-    toggleSpinner(toggle) {
-        ts.ui.get(".ts-app", app => {
-            if(toggle) {
-                app.blocking(messages.loading);
-                return;
-            }
-
-            app.done();
-        });
     }
 
     static init() {
